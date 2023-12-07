f9534eb26609bac1f6daa272dbc45649d3795093
Figo Lehman <96614364+Figolehman@users.noreply.github.com>
2023-12-07 18:05:29 +0700

    Initial Commit

---
+++ TechnicalTest/TechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalTestApp.swift
+//  TechnicalTest
+//
+//  Created by Figo Alessandro Lehman on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
